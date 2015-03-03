51b577f10d2dc3773a01af5283a37e0c3857d9d2
Greg Kempe <greg@kempe.net>
2015-03-03 13:17:12 +0200

    Dont redirect on password reset for mobile.

--- lib/mobile_redirect.rb
+++ lib/mobile_redirect.rb
@@ -3,9 +3,12 @@ ApplicationController.class_eval do
   prepend_before_filter :redirect_to_mobile
 
   def redirect_to_mobile
+    # don't redirect on password reset
+    return if request.path =~ /\/users\/password-reset/
+
     ua = UserAgent.parse(request.env['HTTP_USER_AGENT'])
 
-    if params[:nomobile] != "1" && \
+    if params[:nomobile] != "1" and \
       # BB before 10
       (ua.platform == "BlackBerry" and ua.version < "10")\
       or (ua.browser =~ /Nokia/)\
