0823d31e3b0b789a4e93353261cf4960d6973233
Greg Kempe <greg@kempe.net>
2016-01-15 14:46:10 +0200

    Remove Twitter and cllr widget from landing page

--- assets/javascripts/speakup/speakup.js
+++ assets/javascripts/speakup/speakup.js
@@ -40,30 +40,6 @@ $(function($) {
       </nav>\
     </header>\
   </div>\
-\
-  <div id="speakup-homepage" style="display: none">\
-    <div class="container">\
-      <div class="row">\
-        <!-- <div id="speakup-homepage-banner" class="col-sm-8"></div> -->\
-        <div class="col-sm-8">\
-          <h2>Who is Your Ward Councillor?</h2>\
-          <h4>Find out who represents you where you live.</h4>\
-\
-          <form method="get" action="http://info.speakupmzansi.org.za/councillor/" class="cllr">\
-            <label class="form-label" for="cllr_address">What is your address?</label>\
-            <input type="text" class="form-control" id="cllr_address" name="address" value="" style="width: 400px; max-width: 100%">\
-            <input type="submit" class="btn btn-primary" value="Find your councillor">\
-            <input type="button" class="btn btn-default hidden locate" value="Use your browser\'s location">\
-          </form>\
-        </div>\
-\
-        <div class="col-sm-4 hidden-xs">\
-          <a class="twitter-timeline" height="250" href="https://twitter.com/speakupmzansi" data-widget-id="534284291811721217">Tweets by @SpeakUpMzansi</a>\
-          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>\
-        </div>\
-      </div>\
-    </div>\
-  </div>\
 </div>';
 
     self.init = function() {
@@ -75,12 +51,6 @@ $(function($) {
       Discourse.PageTracker.current().on('change', function(url) {
         self.toggleHomepage(url);
       });
-
-      if (navigator.geolocation) {
-        $('.cllr .btn.locate')
-          .removeClass('hidden')
-          .on('click', self.councillorGeolocate);
-      }
     };
 
     self.injectContent = function() {
@@ -98,25 +68,6 @@ $(function($) {
                  (url.split('/').length <= 2);
 
       $('#speakup-homepage').toggle(show);
-    };
-
-    self.councillorGeolocate = function(e) {
-      var $btn = $('.cllr .btn.locate');
-
-      function foundLocation(position) {
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
-        $('form.cllr [name=address]').val(lat + ',' + lng);
-        $('form.cllr').submit();
-      }
-
-      function noLocation() {
-        $btn.val('Use your location');
-        alert('Sorry, your browser was unable to determine your location.');
-      }
-
-      $btn.val('Locating...');
-      navigator.geolocation.getCurrentPosition(foundLocation, noLocation, {timeout:10000});
     };
   };
 
